037c10e98c597fc4c498df4cb97492b8e5270c18
ShuaiZhang-kul <shuai.zhang@student.kuleuven.be>
2021-07-05 12:19:51 +0200

    new version

--- src/Detection.java
+++ src/Detection.java
@@ -1,36 +1,43 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Detection {
     public static void main(String[] args) throws IOException {
-        long timeStart = System.currentTimeMillis();
-        File file = new File("C:\\Users\\shuai\\Desktop\\test.txt");//文件路径
+        File file = new File("C:\\Users\\shuai\\Desktop\\outputfile4600-5260");//文件路径
         FileReader fileReader = new FileReader(file);
         LineNumberReader reader = new LineNumberReader(fileReader);
-        int number = 9999999;//设置指定行数
         String txt = "initial";
         int lines = 0;
+        Boolean zeroFlag=false;
         System.out.println("Starting to read the file");
         System.out.println("---------------------------------------");
 
         while (txt != null) {
-
             lines++;
+            //new line scanned will start here
             txt = reader.readLine();
+            //avoid null file
             if (txt==null){ break;}
-//                System.out.println("第" + reader.getLineNumber() + "的内容是：" + txt + "\n");
-                String [] column = txt.split(" ");
-                if (column[column.length-1].equals("0")){
-                    System.out.println("there is an arrow at line "+lines+", please check");
-                    System.exit(0);
+            //split this row into multiple columns
+            List<String> columns = Arrays.asList(txt.split(" "));
+            int length =columns.size();
+            //here zero value detected therefore set the flag to find the timestep next
+            if (columns.lastIndexOf("0")== columns.size()-1&&columns.size()!=0){
+                    System.out.println("there is an zero at line "+lines+", please check");
+                    zeroFlag=true;
+//                    System.exit(0);
                 }
-
-
+            //here use the flag to detect the time step by using the keyword of "Flow"
+            if (zeroFlag==true && columns.contains("Flow")){
+                System.out.println("The time step is "+columns.get(columns.size()-1));
+                zeroFlag=false;
+            }
 
         }
 
         System.out.println("---------------------------------------");
         System.out.println("Reading file finished");
-        System.out.println("There is no error");
         reader.close();
         fileReader.close();
     }
